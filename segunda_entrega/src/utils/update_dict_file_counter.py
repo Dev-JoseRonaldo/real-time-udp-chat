6951604da7c9a7c95d70e75eba22022ead5bc3e7
José Ronaldo <dev.joseronaldo@gmail.com>
2024-03-10 16:43:38 -0300

    refactor: adjuste 3-way-handshake and outputs

--- segunda_entrega/src/utils/update_dict_file_counter.py
+++ segunda_entrega/src/utils/update_dict_file_counter.py
@@ -1,5 +1,3 @@
-import os
-
 client_dict_file_counter = {}
 server_dict_file_counter = {}
 
@@ -12,11 +10,3 @@ def update_dict_file_counter(dict_file_counter, user):
     else:
         dict_file_counter[user] = 1
     return dict_file_counter
-
-def remove_user_files(dict_file_counter, user, environment):
-    file_counter = dict_file_counter[user]
-    for count in range(1, file_counter+1):
-        file_name = f"{user}{count}"
-        os.remove(f"./segunda_entrega/data/{environment}/{file_name}.txt")
-
-    del dict_file_counter[user]
